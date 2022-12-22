d3e9aacfb1b47c77192660425a4c61b9371b7058
zaryabriaz <zaryabriaz@gmail.com>
2022-12-23 01:10:20 +0500

    React initial structure and homepage leftside

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Header } from '../Header/Header'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header/>
+        {/* the best ad */}
+        <div className="the-best-ad">
+            <div></div>
+            <span>
+              the best fitness club in the town
+            </span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>SHAPE</span>
+            <span>Your</span>
+          </div>
+          <div><span>Ideal body</span></div>
+
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and live up your life to fullest
+            </span>  
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* Heros button */}
+        <div className="hero-buttons">
+          <button className="btn">
+          Get Started
+          </button>
+          <button className="btn">
+          Learn More
+          </button>
+        </div>
+
+      </div>
+
+
+      <div className="right-h">right side</div>
+    </div>
+  )
+}
+
+export default Hero
