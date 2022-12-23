68dbdecd2f8b63af60777d1d97043c5e38e1828a
zaryabriaz <zaryabriaz@gmail.com>
2022-12-23 19:03:55 +0500

    Added testimonial,reasons, plans,join and footer part of main page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Footer.css'
+import Github from '../../assets/github.png'
+import Instagram from '../../assets/instagram.png'
+import LinkedIn from '../../assets/linkedin.png'
+import Logo from '../../assets/logo.png'
+
+export const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={LinkedIn} alt="" />
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
